c7a10e39a12cd44a31980bce2bf04ed4d38d6804
Hacker God <jyrone.parker@gmail.com>
2024-11-19 23:21:01 -0500

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,35 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', ['@nuxtjs/google-adsense', {
     id: 'ca-pub-7023023584987784',
-    onPageLoad: true,
-    pageLevelAds: true,
-  }]],
+    onPageLoad: false,
+    pageLevelAds: false,
+  }], '@vite-pwa/nuxt'],
   ssr: false,
   app: {
     baseURL: '/watermark-app/', // Replace with your repository name for GitHub Pages
+  },
+  pwa: {
+    registerType: 'autoUpdate', // Automatically update the service worker
+    manifest: {
+      name: 'Nuxt Watermark Creator',
+      short_name: 'Watermark',
+      description: 'Create image and text watermarks easily with Nuxt.',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/watermark-app',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
   },
 })
